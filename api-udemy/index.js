a5544142fee0ff0b34ebe9b8a2c63fcf01c65946
hakua <hakua1452@gmail.com>
2023-06-20 07:04:30 +0900

    WebApi getメソッド

--- api-udemy/index.js
+++ api-udemy/index.js
@@ -1,1 +1,23 @@
-console.log("nodejs");
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+const customers = [
+  {title: '山田', id: 1 },
+  {title: '鈴木', id: 2 },
+  {title: '佐藤', id: 3 },
+  {title: '高橋', id: 4 },
+  {title: '田中', id: 5 },
+]
+
+app.get('/api/customers', (req, res) => {
+  res.send(customers);
+})
